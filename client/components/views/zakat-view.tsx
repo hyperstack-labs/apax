a79f5079afcdb4057ae2be7adb04d5e1166b4ef8
Stephen01623 <ralphjoedg@gmail.com>
2026-02-16 13:52:19 +0800

    fix: error

--- client/components/views/zakat-view.tsx
+++ client/components/views/zakat-view.tsx
@@ -1,6 +1,6 @@
 'use client'
 import jsPDF from 'jspdf'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Calculator, Info, CheckCircle, Coins, TrendUp, Heart } from '@phosphor-icons/react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
